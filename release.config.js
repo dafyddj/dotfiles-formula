7ba8e8c42e9631af47fe651f08662f799224404c
Dafydd Jones <dafydd@techneg.it>
2025-08-01 17:18:53 +0100

    ci(sem-rel): choose `basic` release style

--- release.config.js
+++ release.config.js
@@ -7,17 +7,6 @@ module.exports = {
       releaseRules: './release-rules.js'
     }],
     '@semantic-release/release-notes-generator',
-    ['@semantic-release/changelog', {
-      changelogFile: 'CHANGELOG.md',
-      changelogTitle: '# Changelog'
-    }],
-    ['@semantic-release/exec', {
-      // eslint-disable-next-line no-template-curly-in-string
-      prepareCmd: 'sh ./pre-commit_semantic-release.sh ${nextRelease.version}'
-    }],
-    ['@semantic-release/git', {
-      assets: ['*.md', 'docs/*.rst', 'FORMULA']
-    }],
     '@semantic-release/github'
   ],
   generateNotes: {
